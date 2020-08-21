d85c05f2e812df0ceeb814715c1d8ada00fa6f88
Sylvie <sylvie.z.lu@gmail.com>
2020-08-21 12:08:57 +0200

    changed PORT

--- server.js
+++ server.js
@@ -1,43 +1,43 @@
 // Importation modules npm
-const express = require('express')
-const exphbs = require('express-handlebars')
-const bodyParser = require("body-parser");
-const passport = require("passport");
-const session = require("express-session");
-const flash = require("express-flash");
+const express = require('express');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const session = require('express-session');
+const flash = require('express-flash');
 
 // Instanciation de la fonction dans le fichier passport.js
-const initializePassport = require("./config/passport");
+const initializePassport = require('./config/passport');
 
 // Instanciation d'express
-const app = express()
+const app = express();
 
 // Appel de la fonction initializePassport
 initializePassport(passport);
 
 // Instanciation du port localhost
-const PORT = 8080
+const PORT = process.env.PORT || 8080;
 
 // Configuration moteur de template Handlebars et express.static()
-app.engine('handlebars', exphbs())
-app.set('view engine', 'handlebars')
-app.use(express.static('public'))
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+app.use(express.static('public'));
 
 // MIDDLEWARES
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(flash());
 
 // SESSION
 app.use(
-session({
-    secret: "keyboard cat",
+  session({
+    secret: 'keyboard cat',
     resave: false,
     saveUninitialized: false,
     cookie: {
-        maxAge: 86400000, // 24 hours
+      maxAge: 86400000, // 24 hours
     },
-})
+  })
 );
 
 // PASSPORT
@@ -45,12 +45,12 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // ROUTES
-app.use(require('./routes/authRoutes.js'))
-app.use(require('./routes/homeRoute.js'))
-app.use(require('./routes/dashboardRoute.js'))
-app.use(require('./routes/error404Route.js'))
+app.use(require('./routes/authRoutes.js'));
+app.use(require('./routes/homeRoute.js'));
+app.use(require('./routes/dashboardRoute.js'));
+app.use(require('./routes/error404Route.js'));
 
 // Application Listen (http://localhost:8080/)
 app.listen(PORT, () => {
-    console.log(`Serveur lancé sur le port ${PORT} 🚀`)
+  console.log(`Serveur lancé sur le port ${PORT} 🚀`);
 });
